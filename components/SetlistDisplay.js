ec0d3b7aa0810edcc6e92a789eabaee4c97babdf
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-06-09 17:55:12 -0400

    Updated styling

--- components/SetlistDisplay.js
+++ components/SetlistDisplay.js
@@ -1,25 +1,38 @@
 import AliceCarousel from 'react-alice-carousel';
 import Song from './Song';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import SetlistNavigation from './SetlistNavigation';
+import { useState } from 'react';
 
 export default function SetlistDisplay({ setlist }) {
+  const [songIndex, setSongIndex] = useState(0);
   function buildTemplates() {
     return setlist?.songs?.map(song => <Song song={song} key={song} />);
   }
 
   if (setlist) {
     return (
-      <div className="max-w-3xl mx-auto px-10 md:px-0 py-3 whitespace-pre">
-        <AliceCarousel
-          swipeDelta={100}
-          animationDuration={400}
-          disableButtonsControls
-          disableDotsControls
-          disableSlideInfo
-          items={buildTemplates()}
-          mouseTracking={true}
+      <>
+        <div className="max-w-3xl mx-auto px-10 md:px-0 py-3 whitespace-pre mb-20">
+          <AliceCarousel
+            swipeDelta={100}
+            animationDuration={400}
+            disableButtonsControls
+            disableDotsControls
+            disableSlideInfo
+            items={buildTemplates()}
+            mouseTracking={true}
+            onSlideChanged={e => setSongIndex(e.slide)}
+            activeIndex={songIndex}
+            autoHeight
+          />
+        </div>
+        <SetlistNavigation
+          songs={setlist?.songs}
+          index={songIndex}
+          onIndexChange={setSongIndex}
         />
-      </div>
+      </>
     );
   } else {
     return null;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 module.exports = {
-	mode: "jit",
-	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-	darkMode: "class", // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'dark-gray-900': '#010409',
+        'dark-gray-800': '#0d1117',
+        'dark-gray-700': '#161b22',
+        'dark-gray-600': '#21262d',
+        'dark-gray-400': '#30363d',
+        'dark-gray-200': '#8b949e',
+        'dark-gray-100': '#c9d1d9',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
