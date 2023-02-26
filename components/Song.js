5264dabff3497c6909d2ee2738889fe56b5592bb
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2023-02-25 22:31:03 -0500

    use chord-kit

--- components/Song.js
+++ components/Song.js
@@ -1,12 +1,14 @@
-import { removeChords } from '../lib/SongUtils';
+import { build } from '@cadencechords/chord-kit';
 
 export default function Song({ song }) {
+  const content = build({
+    content: song.content,
+    format: { chords_hidden: true },
+  });
   return (
     <div className="min-w-full">
-      <h2 className="mb-4 font-semibold text-xl">{song.name}</h2>
-      <div className="text-lg whitespace-pre-wrap">
-        {removeChords(song.content)}
-      </div>
+      <h2 className="mb-4 text-xl font-semibold">{song.name}</h2>
+      <div className="text-lg whitespace-pre-wrap">{content}</div>
     </div>
   );
 }
